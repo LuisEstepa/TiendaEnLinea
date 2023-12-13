80b532fc35f528b3f0c1af28faebfa0e89f7788f
BelfastA <angelblack_41@hotmail.com>
2023-12-12 20:34:33 -0500

    Compiler

--- Tienda/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Tienda/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0276150644b068332b026bf065f4efe9cd6ccd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9995663acf8bbd5fd6bc97756a29a9bc9edb450b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
